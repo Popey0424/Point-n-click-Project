e905576a3ec9f82abb12b14d1ebd4a67f80b1db6
DESKTOP-N4F36O6\roylo <roylouis0424@gmail.com>
2024-11-07 00:17:36 +0100

    Ajout de Lights, Cast de Shadow, et Chapter Scene, effect de camera

---
+++ Assets/ChapterScene.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class ChapterScene : MonoBehaviour
+{
+    [Header("Chapter Settings")]
+    [SerializeField] private Image imageFade;
+
+    private void Start()
+    {
+        imageFade.DOFade(0, 2.9f).OnComplete(ResetFade);
+    }
+    private void ResetFade()
+    {
+        StartCoroutine(LaunchChapter());
+    }
+
+    IEnumerator LaunchChapter()
+    {
+        yield return new WaitForSeconds(3f);
+        imageFade.DOFade(1, 2.5f).OnComplete(FadeCompleted);
+    }
+
+    private void FadeCompleted()
+    {
+        SceneManager.LoadScene("MainGame");
+        Debug.Log("SceneLancer");
+    }
+
+
+    
+}
