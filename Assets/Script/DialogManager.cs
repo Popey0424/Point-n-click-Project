059ab983837fe0c81367776b2dcc91067706aab0
Popey0424 <roylouis0424@gmail.com>
2024-11-08 16:33:30 +0100

    Debut de séquence Dialog

--- Assets/Script/DialogManager.cs
+++ Assets/Script/DialogManager.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class TextDialog
+{
+    public string[] TextString;
+    public int InteractionID;
+    public Button ContinueText;
+}
 
 public class DialogManager : MonoBehaviour
 {
+    [Header("List")]
+    [SerializeField] private List<TextDialog> dialogList = new List<TextDialog>();
+
     [Header("Dialogue Settings")]
     [SerializeField] private GameObject continueButton;
     [SerializeField] private float typingSpeed = 0.05f;
     [SerializeField] private TextMeshProUGUI textDialog;
     [SerializeField] private string currentline;
+    [SerializeField] private int textId;
 
     [Header("Debug")]
     [SerializeField] private bool isTyping;
@@ -19,34 +33,37 @@ public class DialogManager : MonoBehaviour
 
     private void Start()
     {
+
         continueButton.SetActive(false);
     }
-
     private void Update()
     {
-        
+       
     }
 
 
-    public void StartDialogue(string[] dialogueLines)
-    {
-        StartCoroutine(DisplayLines(dialogueLines));
-    }
 
-    IEnumerator DisplayLines(string[] lines)
-    {
-        foreach (string line in lines)
-        {
-            yield return StartCoroutine(TypeLine(line));
-            continueButton.SetActive(true);  
-            yield return new WaitUntil(() => Input.GetMouseButtonDown(0)); 
-            continueButton.SetActive(false);
-        }
-    }
+    //public void StartDialogue(string[] dialogueLines)
+    //{
+    //    StartCoroutine(DisplayLines(dialogueLines));
+    //}
 
-    IEnumerator TypeLine(string line)
-    {
-        yield return new WaitForSeconds(typingSpeed);
-    }
+    //IEnumerator DisplayLines(string[] lines)
+    //{
+    //    foreach (string line in lines)
+    //    {
+    //        yield return StartCoroutine(TypeLine(line));  
+           
+    
+    //    }
+    //}
+
+    //IEnumerator TypeLine(string line)
+    //{
+    //    yield return new WaitForSeconds(typingSpeed);
+    //}
+
+
+    
 
 }
